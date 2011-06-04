5d4c38b34128103c9d4f1135e5b4e0dc7f2537a1
oliviermuylle@gmail.com <oliviermuylle@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-04 12:31:16 +0000

    Add of unspecall command
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk@34 32555feb-acdc-9533-d4e0-d00e80c872cf

--- python_bot/commands.py
+++ python_bot/commands.py
@@ -139,6 +139,12 @@ def f(server_write, args, **_):
 server['specall'] = f
 
 def f(server_write, args, **_):
+	server_write('''\
+			code:server.unspecall(true)\
+			''')
+server['unspecall'] = f
+
+def f(server_write, args, **_):
     server_write('''\
 			code:local cn = tonumber('%s');\
 			if server.valid_cn(cn) then\
