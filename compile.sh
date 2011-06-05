f072e4988733777a6fc5f87eb528ed2faac331cd
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-05 16:59:20 +0000

    Improved compile.sh
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk@59 32555feb-acdc-9533-d4e0-d00e80c872cf

--- compile.sh
+++ compile.sh
@@ -1,30 +1,36 @@
-#!/bin/sh
+#!/bin/bash
+PROJECT="$(tput bold ; tput setaf 3)SuckerServ$(tput sgr0)"
 THREADS=`cat /proc/cpuinfo | grep processor | wc -l`
-ARG_LENGTH=$# 
-STRCOMPILE="Compiling"
+ARG_LENGTH=$#
+if [[ $ARG_LENGTH > 2 ]]; then
+  echo "Usage: $0 [--recompile] [--debug] - Build SuckerServ"
+  echo "    --recompile    Delete \$COMPILEDIR before compiling SuckerServ again" 
+  echo "    --debug        Make a debug build"
+fi
+STRCOMPILE="$(tput bold ; tput setaf 2)Compiling$(tput sgr0)"
 COMPILEDIR="release_build"
 COMPILEFLAGS=""
-BUILDTYPE="release"
-if [ "$ARG_LENGTH" > 1 -a "$2" = "debug" ]; then
+BUILDTYPE="$(tput bold ; tput setaf 6)release$(tput sgr0)"
+if [ "$ARG_LENGTH" > 0 -a "$1" = "--debug" -o "$2" = "--debug" ]; then
   COMPILEDIR="debug-build"
   COMPILEFLAGS="-D CMAKE_BUILD_TYPE=DEBUG"
-  BUILDTYPE="debug"
+  BUILDTYPE="$(tput bold ; tput setaf 1)debug$(tput sgr0)"
 fi
-if [ "$ARG_LENGTH" > 0 -a "$1" = 1 ]; then
-  STRCOMPILE="Recompiling"
+if [ "$ARG_LENGTH" > 0 -a "$1" = "--recompile" -o "$2" = "--recompile" ]; then
+  STRCOMPILE="$(tput bold ; tput setaf 5)Recompiling$(tput sgr0)"
   rm -rf $COMPILEDIR
 fi
-mkdir $COMPILEDIR
+if [ $THREADS = 1 ]; then
+  echo "Unable to detect number of threads, using 1 thread."
+  THREADS=1
+fi
+if [ ! -d $COMPILEDIR ]; then
+  mkdir $COMPILEDIR
+fi
 cd $COMPILEDIR
-cmake $COMPILEFLAGS .. >> /dev/null
 STRTHREADS="threads"
 if [ $THREADS = 1 ]; then
   STRTHREADS="thread"
 fi
-echo "$STRCOMPILE Hopmod using $THREADS $STRTHREADS ($BUILDTYPE build)\n"
-TS_START=`date +%s`
-make -j$THREADS 
-make install >> /dev/null
-TS_END=`date +%s`
-TS_DIFF=`echo $TS_END $TS_START | awk '{print $1 - $2}'`
-echo "\nTook $TS_DIFF Seconds"
+echo "$STRCOMPILE $PROJECT using $(tput bold ; tput setaf 4)$THREADS$(tput sgr0) $STRTHREADS ($BUILDTYPE build)"
+time (cmake $COMPILEFLAGS .. ; make -j$THREADS install)
