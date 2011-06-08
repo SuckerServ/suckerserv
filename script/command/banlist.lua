89c75f8b33d69b3f59903c360b6e0d7ee0568784
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-08 17:03:27 +0000

    Exported #banlist message
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk@65 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/banlist.lua
+++ script/command/banlist.lua
@@ -10,7 +10,7 @@ return function(cn)
 					vars.ban_name = ""
 				end
 				local ban_min = round((vars.ban_expire - os.time()) / 60)
-				server.player_msg(cn, string.format(blue() .. "Player: " .. red() .. vars.ban_name .. " \f1IP: " .. red() .. ipmask .. blue() " Reason: " .. red() .. vars.ban_reason .. blue() " Time: " .. red() .. vars.ban_time .. blue() " Admin: " .. red() .. vars.ban_admin .. blue() " Expires in: " .. red() .. ban_min .. " Minutes"))
+				server.player_msg(cn, string.format(server.banlist_message, vars.ban_name, ipmask, vars.ban_reason, vars.ban_time, vars.ban_admin, ban_min))
 			end
 		end
     end	
