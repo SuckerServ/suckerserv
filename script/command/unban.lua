384f736f8353e976e087ae6c4f6724a4b3c22969
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-08 18:50:22 +0000

    New simplist #unban command and modified some messages
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk@67 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/unban.lua
+++ script/command/unban.lua
@@ -1,9 +1,15 @@
+--[[
+
+	A player command to unban a banned player
+
+]]
+
 return function(cn, ip)
-
-	if tonumber(server.unsetban(ip)) == 1 then
-		server.player_msg(cn, "done, check with #banlist")
-	else
-		server.player_msg(cn, "no matching ban found")
-	end
-
+    if not net.ipmask(ip) then return; end
+    if server.ip_vars(ip).ban_time then
+        server.unban(ip)
+        server.player_msg(cn, string.format(server.unban_message, ip))
+    else
+        server.player_msg(cn, string.format(server.no_matching_ban_message, ip))
+    end
 end
