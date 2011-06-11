26e522a2bea16abd2fc64a781aa217e0ca4865b3
oliviermuylle@gmail.com <oliviermuylle@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-11 18:58:28 +0000

    correction of bugs, again
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk@79 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/_help.lua
+++ script/command/_help.lua
@@ -3,8 +3,8 @@ player_command_descriptions = {
     stats = {"[total]", "Get your current or permanent player stats"},
     msg = {"<cn|name> <message>", "Send a private message to another player"},
     nextmap = {"", "Get the name of the map for the next game"},
-    admin = {"", "Gain admin privilege using auth"}
-	cookie = {"<cn|name>", "Send a this food to your friend"}
+    admin = {"", "Gain admin privilege using auth"},
+	cookie = {"<cn|name>", "Send a this food to your friend"},
 	action = {"msg", "Say what you are actually doing"}
 }
 
