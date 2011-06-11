8d0127f81419d9d76d6d72821dcfc349a2787702
oliviermuylle@gmail.com <oliviermuylle@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-11 11:51:49 +0000

    Adding of the "notice" commande.
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk@70 32555feb-acdc-9533-d4e0-d00e80c872cf

---
+++ script/command/notice.lua
@@ -0,0 +1,17 @@
+return function(cn, ...)
+	local text = ""
+    
+	for _, item in ipairs(arg) do
+		item = tostring(item)
+		if #item > 0 then
+			if #text > 0 then
+				text = text .. " "
+			end
+
+			text = text .. item
+		end
+	end
+    
+    server.msg(string.format(server.notice_message, server.player_displayname(cn), text))
+end
+
