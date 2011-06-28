6f2157a7fd6022ce6c9582e3b275727a143b5923
oliviermuylle@gmail.com <oliviermuylle@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-06-28 17:43:01 +0000

    Adding of a AWARDS listener
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk@160 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/module/irc/python_bot.lua
+++ script/module/irc/python_bot.lua
@@ -307,6 +307,12 @@ server.event_handler("checkmaps", function(cn)
     end
 end)
 
+--[[
+Begin of scripts from LoveForEver
+For Suckerserv
+(c) 2011 
+]]--
+
 server.event_handler("scoreflag", function(cn)
 		sendmsg(string.format(irc_color_blue("%s ")..irc_color_green("scored for team ")..irc_color_blue("%s"), server.player_name(cn), server.player_team(cn)))
 end)
@@ -328,53 +334,6 @@ auth.listener("", function(cn, user_id, domain, status)
     sendmsg(irc_color_red("AUTH: ") .. msg)
 end)
 
---> Get-Best-Stats Listener
-
-local function get_best_stats(time)
-    local players = server.clients()
-    table.sort(players, function(a, b) return server.player_frags(a) > server.player_frags(b) end)
-    local msg = ""
-    local str = ""
-    if time > 0 then
-        str = "GAME STATS"
-    else
-        str = "GAME ENDED"
-    end
-    msg = "7" .. str .. ": ([" .. (server.gamemode or "unknown") .. "/" .. (server.map or "unknown") .. "]/".. #players .. " Players) BEST PLAYERS: ["
-    for i, cn in ipairs(players) do 
-        if i > 2 then break end
-        local format = ""
-		local health = ""
-		if server.gamemode == "ctf" or server.gamemode == "ffa" then
-			health = "/(" .. server.player_health(cn) .. "/" .. server.player_maxhealth(cn) .. ")"
-		end
-        if i == 1 then format = "" end
-        if i == 2 then msg = msg .. "/ " end
-        msg = msg .. string.format("%s%s (%i/%i/%i%s%s)%s", format, server.player_name(cn), server.player_frags(cn), server.player_deaths(cn), server.player_accuracy(cn), "%", health, format)
-        if i == 2 then 
-            msg = msg .. "] " 
-        else
-            msg = msg .. " "
-        end
-    end
-    if #players == 0 then
-        msg = msg .. "No Players] "
-    end
-    if #players == 1 then msg = msg .. "]" end
-    if time > 0 then 
-        msg = msg .. " TIME REMAINING: " .. time .. " Minutes"
-    end
-    return msg
-end
-
-server.event_handler("timeupdate", function(time) -- post game stats each minute
-    if #server.clients() >= 1 and server.irc_display_stats ==1 then
-        sendmsg(get_best_stats(time))
-    end
-    return -1
-end)
-
--- End of Get-Best-Stats Listener
 
 --> MapBattle Listener
 
@@ -416,3 +375,109 @@ server.event_handler("text", function(cn, msg) -- shows wich maps people in-game
 end)
 
 -- End of  Listener
+
+--> Awards Listener
+local stats = server.player_vars_table(server.player_id)
+
+local stats_record_class = {
+    
+    update = function(object, cn, value)
+        
+        if not object.value then
+            object.value = value
+            object.cn = {cn}
+            return
+        end
+        
+        if value < object.value then return end
+        
+        if value == object.value then
+            table.insert(object.cn, cn)
+        else
+            object.value = value
+            object.cn = {cn}
+        end
+    end,
+    
+    list_names = function(object)
+        return print_displaynamelist(object.cn)
+    end,
+    
+    clear = function(object)
+        object.cn = nil
+        object.value = nil
+    end
+}
+
+local best = {
+    frags      = {},
+    kpd        = {},
+    accuracy   = {},
+    takeflag   = {},
+    scoreflag  = {},
+    returnflag = {}
+}
+
+for _, subobject in pairs(best) do
+    setmetatable(subobject, {__index = stats_record_class})
+end
+
+server.event_handler("intermission", function()
+
+    local totalplayercount = server.playercount + server.speccount + server.botcount
+    if totalplayercount < 2 then return end
+	
+	local function update_stats(player)
+    
+        local cn = player.cn
+        local participated = player:frags()
+        
+        if participated then
+            best.accuracy:update(cn, player:accuracy())
+            best.frags:update(cn, player:frags())
+            best.kpd:update(cn, player:frags() - player:deaths())
+			
+			local player_stats = stats.vars(cn)
+			best.takeflag:update(cn, player_stats.takeflag or 0)
+            best.scoreflag:update(cn, player_stats.scoreflag or 0)
+            best.returnflag:update(cn, player_stats.returnflag or 0)
+        end
+	end
+	
+    for p in server.gplayers() do
+        update_stats(p)
+    end
+    
+    for b in server.gbots() do
+	update_stats(b)
+    end
+    
+    if best.kpd.value then
+        local cn = best.kpd.cn[1]
+        best.kpd.value = round((server.player_frags(cn)+1)/(server.player_deaths(cn)+1), 2)
+    end
+    
+    local function format_message(record_name, record, append)
+        if not record.value or #record.cn > 2 then return "" end
+        append = append or ""
+        return irc_color_red(string.format("%s: %s", record_name, irc_color_blue(record:list_names()) .. " " .. irc_color_pink(record.value .. append)))
+    end
+    
+	local stats_message = print_list(
+        format_message("frags", best.frags),
+        format_message("kpd", best.kpd),
+        format_message("accuracy", best.accuracy, "%"),
+		format_message("flag scorere", best.scoreflag))
+        
+    if #stats_message > 0 then
+    sendmsg(string.format(irc_color_orange("AWARDS ").."%s", stats_message))
+    end
+	
+    stats.clear()
+    
+    for _, record in pairs(best) do
+        record:clear()
+    end
+end)
+
+-- end of Awards listener
