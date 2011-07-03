75ae684b4da062e2e5481479260523b45ac184a4
aaronleem@gmail.com <aaronleem@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-07-03 22:49:34 +0000

    Added reload message
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@212 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/admin/reload.lua
+++ script/command/admin/reload.lua
@@ -1,4 +1,6 @@
 -- [[ based on a player command written by Thomas ]] --
 
-return server.reload_lua
-
+return function(cn)
+	server.reload_lua
+	server.msg(string.format(concat ((red ">>>") "Server has been" (blue "rehashed") "by" (blue"%s")), server.player_displayname(cn)))
+end
