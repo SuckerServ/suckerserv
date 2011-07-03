5c25947a5b3db13462a0ca86454d9d02153314b3
aaronleem@gmail.com <aaronleem@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-07-03 22:56:45 +0000

    changed message function
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@215 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/admin/reload.lua
+++ script/command/admin/reload.lua
@@ -2,5 +2,5 @@
 
 return function(cn)
 	server.reload_lua
-	server.msg(string.format(concat ((red ">>>") "Server has been" (blue "rehashed") "by" (blue"%s")), server.player_displayname(cn)))
+	server.msg(string.format(server.reload_message, server.player_displayname(cn)))
 end
