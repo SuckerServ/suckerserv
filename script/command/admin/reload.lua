4cc2bd950407b3f561a33d67950242b76d45c257
oliviermuylle@gmail.com <oliviermuylle@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-07-05 07:33:07 +0000

    Correcting commant back to old script
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@218 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/admin/reload.lua
+++ script/command/admin/reload.lua
@@ -1,6 +1,3 @@
 -- [[ based on a player command written by Thomas ]] --
 
-return function(cn)
-	server.reload_lua
-	server.msg(string.format(server.reload_message, server.player_displayname(cn)))
-end
+return server.reload_lua
