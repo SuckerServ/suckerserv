2c0c952bf2ab66f5056c32a2a9f1cbda47378963
bram.wall@gmail.com <bram.wall@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-07-05 11:45:38 +0000

    fixed message coloring and newline bug
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@223 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/me.lua
+++ script/command/me.lua
@@ -1,7 +1,17 @@
 --[[
     Say what you are doing actually 
     By LoveForever
+    escape function added by killme_nl
 ]]
+local function cs_escape(msg)
+  --function to escape newlines and message coloring
+  msg = string.gsub(msg, "\f", "f")
+  msg = string.gsub(msg, "^f", "f")
+  msg = string.gsub(msg, "\n", "n")
+  msg = string.gsub(msg, "^n", "n")
+  return msg
+end
+
 return function(cn, ...)
 	local text = ""
     
@@ -15,7 +25,7 @@ return function(cn, ...)
 			text = text .. item
 		end
 	end
-    
+  text = cs_escape(text)
     server.msg(string.format(server.me_message, server.player_displayname(cn), text))
 end
 
