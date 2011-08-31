cf070712a81f7bf847b708dff3c4b016b4334a6a
oliviermuylle@gmail.com <oliviermuylle@gmail.com@32555feb-acdc-9533-d4e0-d00e80c872cf>
2011-08-31 19:52:38 +0000

    Back to best stats + adding colors + changing name to awards
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@320 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/module/display/awards.lua
+++ script/module/display/awards.lua
@@ -1,15 +1,16 @@
 --[[
-    Display awards at intermission
+    Display best player stats at intermission
     
-    Copyright (C) 2011 LoveForEver
-	
-	Inspired bu best_stats script from Hopmod
+    Copyright (C) 2009 Graham Daws
+
+    01/08/2011 Thomas: use score() instead of frags(), better kpd calculation, use client way for accuracy
 ]]
+
 local stats = server.player_vars_table(server.player_id)
 
 local stats_record_class = {
     
-    update = function(object, cn, value)
+    update = function(object, cn, value, desc)
         
         if not object.value then
             object.value = value
@@ -17,7 +18,11 @@ local stats_record_class = {
             return
         end
         
-        if value < object.value then return end
+        if desc ~= nil then
+            if value > object.value then return end 
+        else
+            if value < object.value then return end
+        end
         
         if value == object.value then
             table.insert(object.cn, cn)
@@ -43,7 +48,8 @@ local best = {
     accuracy   = {},
     takeflag   = {},
     scoreflag  = {},
-    returnflag = {}
+    returnflag = {},
+    timetrial  = {}
 }
 
 for _, subobject in pairs(best) do
@@ -54,24 +60,37 @@ server.event_handler("intermission", function()
 
     local totalplayercount = server.playercount + server.speccount + server.botcount
     if totalplayercount < 2 then return end
-	
-	local function update_stats(player)
+    
+    local check_ctf_stats = gamemodeinfo.ctf
+    
+    local function update_stats(player)
     
         local cn = player.cn
-        local participated = player:score()
+        local qualify = player:score() > round(server.gamemillis / 60000); -- game duration in mins
         
-        if participated then
+        if qualify then
             best.accuracy:update(cn, player:accuracy2())
             best.frags:update(cn, player:score())
             best.kpd:update(cn, player:score() - player:deaths())
-			
-			local player_stats = stats.vars(cn)
-			best.takeflag:update(cn, player_stats.takeflag or 0)
+        end
+        
+        if check_ctf_stats then
+            
+            local player_stats = stats.vars(cn)
+            
+            best.takeflag:update(cn, player_stats.takeflag or 0)
             best.scoreflag:update(cn, player_stats.scoreflag or 0)
             best.returnflag:update(cn, player_stats.returnflag or 0)
+
+            local timetrial = player_stats.timetrial or 0
+            if timetrial > 0 then
+                best.timetrial:update(cn, timetrial, true)
+            end
+
         end
-	end
-	
+        
+    end
+    
     for p in server.gplayers() do
         update_stats(p)
     end
@@ -94,19 +113,52 @@ server.event_handler("intermission", function()
         return blue(string.format("%s: %s", record_name, white(record:list_names()) .. " " .. red(record.value .. append)))
     end
     
-	local stats_message = print_list(
+    local stats_message = print_list(
         format_message("frags", best.frags),
         format_message("kpd", best.kpd),
-        format_message("accuracy", best.accuracy, "%"),
-		format_message("flag scorere", best.scoreflag))
+        format_message("accuracy", best.accuracy, "%"))
         
     if #stats_message > 0 then
-    server.msg(string.format(server.awards_message, stats_message))
+        server.msg(string.format(server.awards_stats_message, stats_message))
     end
-	
+    
+    if check_ctf_stats then
+        
+        local flagstats_message = print_list(
+            format_message("stolen", best.takeflag), 
+            format_message("scored", best.scoreflag),
+            format_message("returned", best.returnflag),
+            format_message("flagrun", best.timetrial, " ms"))
+        
+        if #flagstats_message > 0 then
+			server.msg(string.format(server.awards_flags_message, flagstats_message))
+        end
+    end
+    
     stats.clear()
     
     for _, record in pairs(best) do
         record:clear()
     end
 end)
+
+server.event_handler("takeflag", function(cn)
+    local player_stats = stats.vars(cn)
+    player_stats.takeflag = (player_stats.takeflag or 0) + 1
+end)
+
+server.event_handler("scoreflag", function(cn, _, __, timetrial)
+    local player_stats = stats.vars(cn)
+    player_stats.scoreflag = (player_stats.scoreflag or 0) + 1
+    player_stats.timetrial = (player_stats.timetrial or 0)
+    if timetrial == 0 then return end
+    if player_stats.timetrial == 0 or player_stats.timetrial > timetrial then
+        player_stats.timetrial = timetrial
+    end
+end)
+
+server.event_handler("returnflag", function(cn)
+    local player_stats = stats.vars(cn)
+    player_stats.returnflag = (player_stats.returnflag or 0) + 1
+end)
+
