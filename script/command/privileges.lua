fc2be250224262aa396ecffd1b52163be0e28371
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2012-02-11 19:58:49 +0000

    Added city to script/command/players.lua + added a privileges command to display players with privileges
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@428 32555feb-acdc-9533-d4e0-d00e80c872cf

---
+++ script/command/privileges.lua
@@ -0,0 +1,11 @@
+--[[
+	A player command to list players privileges
+]]
+
+return function(cn)
+    for p in server.gplayers() do
+        if server.player_priv_code(p.cn) > server.PRIV_NONE then
+            server.player_msg(cn, string.format(server.player_privileges_list_message, p:name(), p.cn, p:priv()))
+        end
+    end
+end
