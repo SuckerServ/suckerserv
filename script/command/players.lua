bfe7cea35bf1812fd9d5d7519dbe53531431967a
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2012-02-11 19:58:49 +0000

    Added city to script/command/players.lua + added a privileges command to display players with privileges
    git-svn-id: https://suckerserv.googlecode.com/svn/branches/suckerserv-v5@428 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/players.lua
+++ script/command/players.lua
@@ -4,7 +4,11 @@
 
 return function(cn)
     for p in server.gplayers() do
-        server.player_msg(cn, string.format(server.player_list_message, p:displayname(), geoip.ip_to_country(p:ip()), p:frags(), p:deaths(), p:accuracy()))
+        local country = geoip.ip_to_country(p:ip())
+        local city = geoip.ip_to_city(p:ip())
+        if not country or #country < 1 then city = "Unknown" end
+        if not city or #city < 1 then city = "Unknown" end
+        server.player_msg(cn, string.format(server.player_list_message, p:displayname(), city, country, p:frags(), p:deaths(), p:accuracy()))
     end
 end
 
