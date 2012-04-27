b0d6d2d3088c6df871fd1362f68140c89b1cbe21
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2012-04-27 16:26:52 +0000

    fixed /checkmaps (hopmod rev2573) + fixed /checkmaps another time (hopmod rev2574)
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@452 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/base/modified_map.lua
+++ script/base/modified_map.lua
@@ -1,3 +1,8 @@
+local modified_clients = {}
+function server.modified_map_clients()
+    return modified_clients
+end
+
 local function failed_action(cn)
     server.force_spec(cn)
     modified_clients[server.player_sessionid(cn)] = cn
@@ -13,3 +18,19 @@ server.event_handler("modmap", function(cn, map, crc)
     failed_action(cn)
 
 end)
+
+server.event_handler("disconnect", function(cn)
+    modified_clients[server.player_sessionid(cn)] = nil
+end)
+
+server.event_handler("mapchange", function()
+    modified_clients = {}
+end)
+
+server.event_handler("checkmaps", function(cn)
+    for sessionid, cn in pairs(modified_clients) do
+        server.player_msg(cn, string.format("%s is using a modified map", server.player_displayname(cn)))
+    end
+end)
+
+
