12800e3e386b9712ffa61239a73dc87c7f013e43
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2012-07-08 21:01:00 +0000

    Display all players' privileges in #privileges command instead of displaying only playing ones
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v4@477 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/command/privileges.lua
+++ script/command/privileges.lua
@@ -3,7 +3,7 @@
 ]]
 
 return function(cn)
-    for p in server.gplayers() do
+    for p in server.gall() do
         if server.player_priv_code(p.cn) > server.PRIV_NONE then
             server.player_msg(cn, string.format(server.player_privileges_list_message, p:name(), p.cn, p:priv()))
         end
