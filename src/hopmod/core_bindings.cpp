a1dfbc351e9ec7555fc8ff7dbc0cc129d16a7066
pureascii@gmail.com <pureascii@gmail.com@d7818858-e1b4-11dd-a738-5fe2e3cfbcc5>
2013-02-24 17:02:08 +0000

    Workaround for broken "bind_prop" and cleanup of module "override/default_mastermode"
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2701 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5
    (cherry picked from commit 6b9c555ad68fe4f87cd9a300f7c19e103a5bff95)
    
    Conflicts:
    	src/hopmod/core_bindings.cpp

--- src/hopmod/core_bindings.cpp
+++ src/hopmod/core_bindings.cpp
@@ -170,6 +170,8 @@ void bind_core_functions(lua_State * L, int T)
     bind_function(L, T, "version", hopmod::build_date);
     bind_function(L, T, "build_date", hopmod::build_date);
     bind_function(L, T, "build_time", hopmod::build_time);
+    // pureascii: don't use bind_prop with setter as long as it breaks the getter
+    bind_function(L, T, "set_mastermode", server::set_mastermode);
     
 }
 
@@ -361,8 +363,11 @@ void bind_core_variables(lua_State * L, int T)
     bind_ro_var(L, T, "map", server::smapname);
     bind_var(L, T, "server_password", server::serverpass);
     bind_ro_var(L, T, "master", server::currentmaster);
-    bind_prop(L, T, "timeleft", server::get_minutes_left, server::set_minutes_left);
-    bind_prop(L, T, "seconds_left", server::get_seconds_left, server::set_seconds_left);
+    // pureascii: don't use bind_prop with setter as long as it breaks the getter
+    //bind_prop(L, T, "timeleft", server::get_minutes_left, server::set_minutes_left);
+    //bind_prop(L, T, "seconds_left", server::get_seconds_left, server::set_seconds_left);
+    bind_prop<int>(L, T, "timeleft", server::get_minutes_left, NULL);
+    bind_prop<int>(L, T, "seconds_left", server::get_seconds_left, NULL);
     bind_var(L, T, "intermission", server::interm);
     bind_var(L, T, "intermission_time", server::intermtime);
     bind_ro_var(L, T, "uptime", totalmillis);
@@ -422,6 +427,8 @@ void bind_core_variables(lua_State * L, int T)
     
     bind_var(L, T, "mapcrc", server::mcrc);
 
-    bind_prop<int>(L, T, "mastermode", server::get_mastermode, server::script_set_mastermode);
+    // pureascii: don't use bind_prop with setter as long as it breaks the getter
+    //bind_prop<int>(L, T, "mastermode", server::get_mastermode, server::set_mastermode);
+    bind_prop<int>(L, T, "mastermode", server::get_mastermode, NULL);
 }
 
