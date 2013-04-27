404c81c5f7b67ebcb159afb9e10960ef5389efe2
t.poechtrager@gmail.com <t.poechtrager@gmail.com@d7818858-e1b4-11dd-a738-5fe2e3cfbcc5>
2013-04-27 20:39:09 +0000

    gban module enhancements
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2733 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5
    (cherry picked from commit 1881fe7ccc79e1258461013cbd63f03c45a2362e)

--- script/base/global_bans.lua
+++ script/base/global_bans.lua
@@ -2,59 +2,73 @@ require "http.client"
 require "Json"
 --[[
     You can checkout the bans in a readable form here:
-    
-        http://83.169.44.106/hopmod/view_bans.php
-        
+
+        http://sauer.nomorecheating.org/hopmod/view_bans.php
+
 --]]
 
-local URL = "http://83.169.44.106/hopmod/gbans.php"
-local ADMIN = "global"
-local UPDATE = 1000*60*60*0.5
+local ADMIN_NAME = "global"
+local UPDATE_INTERVAL = 1000*60*60*0.5
+
+local GBAN_MIRRORS =
+{
+    "http://sauer.nomorecheating.org/hopmod/gbans.php",
+    "http://83.169.44.106/hopmod/gbans.php"
+}
+
+local MIRROR = 1
 
 local bans = {}
 
 local function update()
-    http.client.get(URL, function(body, status)
-        
+    http.client.get(GBAN_MIRRORS[MIRROR], function(body, status)
+
         if not body then
-            server.log_error("Failed to download the global ban list: " .. status)
+            server.log_error(string.format("Failed to download the global ban list from %s", GBAN_MIRRORS[MIRROR]))
+
+            if MIRROR < #GBAN_MIRRORS then
+                MIRROR = MIRROR + 1
+                update()
+            end
+
+            return
         end
-        
+
         local data = Json.Decode(body)
-        
+
         local updated_bans = {}
         local change = false
-        
+
         for _, ban in pairs(data) do
-            
+
             updated_bans[ban.address] = true
-            
+
             if not server.is_banned(ban.address) then
                 local bantime = tonumber(ban.expire) or -1
                 if bantime ~= -1 then bantime = bantime - os.time(os.date("!*t")) end
                 local reason = ban.reason or "global ban"
-                server.ban(ban.address, bantime, ADMIN, reason, nil, true)
+                server.ban(ban.address, bantime, ADMIN_NAME, reason, nil, true)
                 change = true
             end
-            
+
             bans[ban.address] = nil
         end
-        
+
         local subtracted_bans = bans
         for address in pairs(subtracted_bans) do
             server.unban(address)
             change = true
         end
-        
+
         bans = updated_bans
-        
+
         if change then
             server.log_status("Updated global bans")
         end
     end)
 end
 
-server.interval(UPDATE, update)
+server.interval(UPDATE_INTERVAL, update)
 update()
 
 server.update_global_bans = update
