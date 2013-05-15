731cfc3217a6ff12713a4f9a78b42aed01fb9456
t.poechtrager@gmail.com <t.poechtrager@gmail.com@d7818858-e1b4-11dd-a738-5fe2e3cfbcc5>
2013-05-15 17:02:48 +0000

    get rid of double declared push functions
    git-svn-id: http://hopmod.googlecode.com/svn/trunk@2759 d7818858-e1b4-11dd-a738-5fe2e3cfbcc5
    (cherry picked from commit 38031043cd22d8380d3f2532158d429755dc99e0)

--- src/hopmod/lua/push_function.cpp
+++ src/hopmod/lua/push_function.cpp
@@ -2,16 +2,6 @@
 
 namespace lua{
 
-void push(lua_State  * L, bool value)
-{
-    lua_pushboolean(L, static_cast<int>(value));
-}
-
-void push(lua_State  * L, int value)
-{
-    lua_pushinteger(L, value);
-}
-
 void push(lua_State * L, long value)
 {
     lua_pushnumber(L, static_cast<lua_Number>(value));
@@ -20,31 +10,6 @@ void push(lua_State * L, long value)
 void push(lua_State * L, unsigned long value)
 {
     lua_pushnumber(L, static_cast<lua_Number>(value));
-}
-
-void push(lua_State  * L, lua_Number value)
-{
-    lua_pushnumber(L, value);
-}
-
-void push(lua_State  * L, lua_CFunction value)
-{
-    lua_pushcfunction(L, value);
-}
-
-void push(lua_State  * L, const char * value)
-{
-    lua_pushstring(L, value);
-}
-
-void push(lua_State  * L, const char * value, std::size_t length)
-{
-    lua_pushlstring(L, value, length);
-}
-
-void push(lua_State  * L, const std::string & value)
-{
-    lua_pushlstring(L, value.data(), value.length());
 }
 
 int to(lua_State * L, int index, return_tag<int>)
