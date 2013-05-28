e58af67efcb4bbfab623390b1699212c1752a6b7
aaron@graphox.us <aaron@graphox.us@32555feb-acdc-9533-d4e0-d00e80c872cf>
2013-05-28 19:21:32 +0000

    updated mapsucks, made it a module
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v5@550 32555feb-acdc-9533-d4e0-d00e80c872cf
    (cherry picked from commit 7d5b6377c95492bce315524e0a70f5848f22d0b3)

--- script/command/mapsucks.lua
+++ script/command/mapsucks.lua
@@ -7,55 +7,10 @@
 
 ]]
 
-local mapsucks = {}
-local plural_time, plural_players, conjugate
-local ratio, lower_time = server.mapsucks_ratio, server.mapsucks_lower_time
-
-server.event_handler("text", function(cn, text)
-  if text:lower():match("sucks") and not text:lower():match("[#!@]mapsucks") then 
-    server.player_msg(cn, server.mapsucks_analysetext_message)
-  end
-end)
-
-server.event_handler("connect", function(cn)
-  cn_id = server.player_id(cn)
-  if not mapsucks[cn_id] then return end
-  mapsucks[cn_id] = cn
-end)
-
-server.event_handler("mapchange", function(map, mode)
-  mapsucks = {}
-end)
-
 return function(cn)
-  for _,player in pairs(server.players()) do
-    if cn == player then
-      cn_id = server.player_id(cn)
-
-      if not mapsucks[cn_id] then
-        mapsucks[cn_id] = cn
-        mapsucks_size = table_size(mapsucks)
-
-        server.player_msg(cn, string.format(server.mapsucks_message, (mapsucks_size - 1)))
-        server.msg(string.format(server.mapsucks_announce_message, server.player_displayname(cn)))
-
-        if mapsucks_size > (#server.players() / ratio) then
-          server.changetime(lower_time*60*1000)
-
-          plural_time = (lower_time > 1) and "s" or ""
-          plural_players = (#server.players() > 1) and "s" or ""
-          conjugate = (#server.players() > 1) and "" or "s"
-
-          server.msg(string.format(server.mapsucks_timelowered_message, lower_time, plural_time, mapsucks_size, #server.players(), plural_players, conjugate))
-        end
-        return
-      else
-        server.player_msg(cn, server.mapbattle_vote_already)
-        return
-      end
-    end
+  if server.player_status_code(cn) ~= server.SPECTATOR then
+    server.mapsucks.vote(cn)
+  else
+    return false, server.mapbattle_cant_vote_message
   end
-
-  server.player_msg(cn, server.mapbattle_cant_vote_message)
-  return -1
 end
