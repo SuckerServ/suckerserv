cbed6f862a355bd627e3d7716098ae912e536ebb
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2013-11-28 20:21:14 +0000

    Show login and priv at connection and fix banner display when GeoIP disabled
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v5@587 32555feb-acdc-9533-d4e0-d00e80c872cf

--- script/base/banner.lua
+++ script/base/banner.lua
@@ -1,7 +1,54 @@
 require "geoip"
 
-local BANNER_DELAY = 1000
+local BANNER_DELAY = 2000
 local show_country_message = server.show_country_message == 1
+
+local function send_connect_message(cn)
+
+    if server.player_is_spy(cn) or server.is_bot(cn) then return end
+
+    local country = geoip.ip_to_country(server.player_ip(cn))
+    local city = geoip.ip_to_city(server.player_ip(cn))
+    if not city or #city < 1 then city = "Unknown" end
+
+
+   if not show_country_message or #country < 1 then
+        country = "Unknown"
+   end
+
+    if server.player_ranking then
+        player_ranking = server.player_ranking(server.player_name(cn))
+    end
+    if not player_ranking then
+        player_ranking = "Unknown"
+    end
+
+    local name = server.player_displayname(cn)
+
+    if server.player_vars(cn).reserved_name then
+        name = green(server.player_name(cn)) .. " (" .. cn .. "): " .. yellow(server.player_vars(cn).reserved_name)
+    elseif server.player_vars(cn).stats_auth_name then
+        name = green(server.player_name(cn)) .. " (" .. cn .. "): " .. yellow(server.player_vars(cn).stats_auth_name)
+    end
+
+    if server.player_priv_code(cn) > server.PRIV_NONE then
+        name = name .. "(" .. server.player_priv(cn) .. ")"
+    end
+
+    local normal_message = string.format(server.client_connect_message, name, city, country, player_ranking)
+    local admin_message = string.format(server.client_connect_admin_message, normal_message, server.player_ip(cn))
+
+    for _, cn in ipairs(server.clients()) do
+
+        local message = normal_message
+
+        if server.player_priv_code(cn) == server.PRIV_ADMIN then
+            message = admin_message
+        end
+
+        server.player_msg(cn, message)
+    end
+end
 
 local function sendServerBanner(cn)
 
@@ -20,43 +67,11 @@ local function sendServerBanner(cn)
         server.player_msg(cn, server.connect_info_message)
 
         server.player_vars(cn).shown_banner = true
+
+        send_connect_message(cn)
     end)
 end
 
-local function onConnect(cn, is_spy)
-
-    if is_spy or server.is_bot(cn) then return end
-    
-    local country = geoip.ip_to_country(server.player_ip(cn))
-    local city = geoip.ip_to_city(server.player_ip(cn))
-    if not city or #city < 1 then city = "Unknown" end
-    
-    if show_country_message and #country > 0 then
-
-        if server.player_ranking then 
-            player_ranking = server.player_ranking(server.player_name(cn)) 
-        end
-        if not player_ranking then
-            player_ranking = "Unknown" 
-        end
-        
-        local normal_message = string.format(server.client_connect_message, server.player_displayname(cn), city, country, player_ranking)
-        local admin_message = string.format(server.client_connect_admin_message, normal_message, server.player_ip(cn))
-        
-        for _, cn in ipairs(server.clients()) do
-            
-            local message = normal_message
-            
-            if server.player_priv_code(cn) == server.PRIV_ADMIN then
-                message = admin_message
-            end
-            
-            server.player_msg(cn, message)
-        end
-    end
-end
-
-server.event_handler("connect",onConnect)
 server.event_handler("maploaded", sendServerBanner)
 server.event_handler("mapchange", function()
     for p in server.gclients() do
