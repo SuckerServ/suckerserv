0bcfe5ea6d8d136575f9b6c1baa64e933681991a
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2013-12-24 18:14:48 +0000

    Add #nodamage command
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v5@605 32555feb-acdc-9533-d4e0-d00e80c872cf

---
+++ script/command/master/nodamage.lua
@@ -0,0 +1,36 @@
+--[[
+	An admin command to disable damages
+]]
+
+local usage = "#nodamage 0|1"
+local nodamage = false
+
+local function init()
+	nodamage = false
+	server.event_handler("damage", function()
+		if nodamage then
+			return -1
+		end
+	end)
+	server.event_handler("mapchange", function() nodamage = false end)
+end
+
+local function unload()
+	nodamage = nil
+end
+
+local function run(cn, option)
+	if not option then
+		return false, usage
+	elseif tonumber(option) == 1 then
+		nodamage = true
+		server.player_msg(cn, server.nodamage_enabled_message)
+	elseif tonumber(option) == 0 then
+		nodamage = false
+		server.player_msg(cn, server.nodamage_disabled_message)
+	else
+		return false, usage
+	end
+end
+
+return {init = init,run = run,unload = unload}
