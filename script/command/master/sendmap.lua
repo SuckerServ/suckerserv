1c266d5000d3120f61640ac403392f49eaae4d57
piernov <piernov@32555feb-acdc-9533-d4e0-d00e80c872cf>
2014-01-02 00:09:23 +0000

    Add #sendmap all to issue #sendmap on all clients
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/suckerserv-v5@609 32555feb-acdc-9533-d4e0-d00e80c872cf
    (cherry picked from commit e1f7028a77685a455eee496c1e60c7199e1711ee)

--- script/command/master/sendmap.lua
+++ script/command/master/sendmap.lua
@@ -2,10 +2,16 @@
 	An admin command for sending the map to a player
 ]]
 
-local usage = "#sendmap <cn>"
+local usage = "#sendmap <cn>|all"
 
 return function(actor, target)
-	if target and server.valid_cn(target) then
+	if target == "all" then
+		for p in server.gclients() do
+			if p.cn ~= actor then
+				server.sendmap(actor, p.cn)
+			end
+		end
+	elseif target and server.valid_cn(target) then
 		server.sendmap(actor, target)
 	else
 		return false, usage
