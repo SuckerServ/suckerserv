2b8b3f620f3d7f7436fef0fee1bf0769cd6817b0
piernov <piernov@piernov.org>
2014-09-13 15:51:19 +0200

    Fix compile.sh for git

--- script/command/version.lua
+++ script/command/version.lua
@@ -3,7 +3,7 @@ return function(cn)
     local version = server.version()
     local revision = server.revision()
     
-    server.player_msg(cn, string.format("Running SuckerServ. %s%s",
+    server.player_msg(cn, string.format("Running SuckerServ-v5. %s%s",
         green("Compiled at: " .. version),
         _if(revision > -1, blue(" Revision: " .. revision), "")
     ))
