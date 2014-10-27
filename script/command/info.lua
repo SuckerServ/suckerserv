a4de46937c8fd131fd14681681edde79af854958
piernov <piernov@piernov.org>
2014-10-27 19:34:27 +0100

    Merge #uptime and #version in #info

--- script/command/info.lua
+++ script/command/info.lua
@@ -1,4 +1,12 @@
-
 return function(cn)
-server.player_msg(cn, string.format(server.info_message))
+    local version = server.version()
+    local revision = server.revision()
+    local uptime = server.format_duration_str(server.uptime)
+    local verstr = ""
+
+    if revision > -1 then
+        verstr = server.parse_message(cn, "version", {version = version, revision = revision})
+    end 
+
+    server.player_msg(cn, "info_command", {uptime = uptime, verstr = verstr})
 end
--- script/command/uptime.lua
+++
@@ -1,5 +0,0 @@
--- [[ Player command written by Thomas
-
-return function(cn)
-    server.player_msg(cn, "uptime", {uptime = server.format_duration_str(server.uptime)})
-end
