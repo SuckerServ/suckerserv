5e79e4665c104978ba89571391f12185a6c2ec69
piernov <piernov@piernov.org>
2014-12-15 22:24:40 +0100

    Display gban list name in #gbans command

--- script/command/admin/gbans.lua
+++ script/command/admin/gbans.lua
@@ -1,13 +1,14 @@
-return function(cn) 
+return function(cn)
     for ipmask, vars in pairs(server.ip_vars()) do
         if (vars.ban_expire or 0) > os.time() and (vars.is_gban or false) then
             server.player_msg(cn, string.format(
-                "IP: %s Reason: %s%s%s",
+                "IP: %s Banlist: %s Reason: %s%s%s",
                 ipmask,
+                vars.ban_admin or "unknown",
                 vars.ban_reason or "unknown",
                 _if(vars.ban_expire - os.time() < 31536000, string.format(blue(" (expires in: %s)"), server.format_duration_str(vars.ban_expire - os.time())), ""),
                 _if(vars.ignore_gban or false, red(" (deleted)"), "")
                         ))
-        end 
+        end
     end
 end
