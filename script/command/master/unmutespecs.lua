17394e93ce937b5a190a34fd5f326e88d60a8007
Patrick Oberdorf <obi12341@googlemail.com>
2015-02-22 17:21:44 +0100

    added text for #unmutespecs

--- script/command/master/unmutespecs.lua
+++ script/command/master/unmutespecs.lua
@@ -1,9 +1,10 @@
 
-return function()
+return function(cn)
     
     if not server.mute_spectators then
         return false, server.missing_mute_spectator_module_message
     end
     
     server.mute_spectators()
+    server.player_msg(cn, server.all_spectator_unmuted_message)
 end
