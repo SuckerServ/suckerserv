7725332ecb18f77a8a663d406f7df6dc81464a0c
Patrick Oberdorf <obi12341@googlemail.com>
2015-02-22 23:59:36 +0100

    Update and rename script/module to script/module/always_fair.lua

---
+++ script/module/always_fair.lua
@@ -0,0 +1,15 @@
+--[[
+    Always start the game fair 
+    By Patrick Oberdorf
+]]
+
+
+server.event_handler("mapchange", function()
+      server.pausegame(true)
+      server.msg(orange("--[ Waiting until all Players loaded the Map."))
+end)
+
+server.event_handler("maploaded", function()
+      server.pausegame(true)
+      server.msg(orange("--[ GO!"))
+end)
