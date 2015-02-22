08d8f64f7ee5b7cabe7370b7338bcebf67f416a6
Patrick Oberdorf <obi12341@googlemail.com>
2015-02-23 00:45:49 +0100

    Update always_fair.lua

--- script/module/always_fair.lua
+++ script/module/always_fair.lua
@@ -4,25 +4,31 @@
 ]]
 
 player_active = {}
+validFunction = false
 
 server.event_handler("mapchange", function()
     player_active = {}
+    validFunction = true
     server.pausegame(true)
     server.msg(orange("--[ Waiting until all Players loaded the Map."))
 end)
 
 server.event_handler("maploaded", function(cn)
     local allactive=true
+    if validFunction then
+        validFunction= false
     
-    player_active[cn]=true
-    for i, cn_ in ipairs(server.players()) do
-        if player_active[cn_] == nil then
-            allactive=false
+        player_active[cn]=true
+        for i, cn_ in ipairs(server.players()) do
+            if player_active[cn_] == nil then
+                allactive=false
+            end
         end
-    end
         
-    if allactive then
-        server.pausegame(false)
-        server.msg(orange("--[ GO!"))
+        if allactive then
+            server.pausegame(false)
+            server.msg(orange("--[ GO!"))
+            validFunction = false
+        end
     end
 end)
