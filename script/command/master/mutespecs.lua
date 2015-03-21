dbf9fb82ffa26c5dc74c3cf7b3550902af08f5b7
Patrick Oberdorf <patrick@oberdorf.net>
2015-03-21 15:00:35 +0100

    added messages for mutespecs and unmutespecs command

--- script/command/master/mutespecs.lua
+++ script/command/master/mutespecs.lua
@@ -1,9 +1,10 @@
 
-return function()
+return function(cn)
     
     if not server.mute_spectators then
         return false, server.missing_mute_spectator_module_message
     end
     
     server.mute_spectators(true)
+    server.player_msg(cn, server.all_spectator_muted_message)
 end
