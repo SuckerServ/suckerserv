1805edf770aa5190c63edabd8f25602dd09e811a
cztk <ztk246@gmail.com>
2015-06-06 14:23:51 +0200

    forgot two files at last commit

--- script/base/banner.lua
+++ script/base/banner.lua
@@ -1,53 +1,57 @@
 require "geoip"
 
-local BANNER_DELAY = 2000
+local BANNER_DELAY = 3000
 local show_country_message = server.show_country_message == 1
 
-local function send_connect_message(cn)
+local function send_connect_message(tcn)
 
-    if server.player_is_spy(cn) or server.is_bot(cn) then return end
+    if server.player_is_spy(tcn) or server.is_bot(tcn) then return end
+    if true == server.player_vars(tcn).shown_connect then return end
 
-    local country = geoip.ip_to_country(server.player_ip(cn))
-    local city = geoip.ip_to_city(server.player_ip(cn))
-    if not city or #city < 1 then city = "Unknown" end
+            local country = geoip.ip_to_country(server.player_ip(tcn))
+            local city = geoip.ip_to_city(server.player_ip(tcn))
+            if not city or #city < 1 then city = "Unknown" end
 
 
-   if not show_country_message or #country < 1 then
-        country = "Unknown"
-   end
+            if not show_country_message or #country < 1 then
+                country = "Unknown"
+            end
 
-    if server.player_ranking then
-        player_ranking = server.player_ranking(server.player_name(cn))
-    end
-    if not player_ranking then
-        player_ranking = "Unknown"
-    end
+            if server.player_ranking then
+                player_ranking = server.player_ranking(server.player_name(tcn))
+            end
 
-    local name = server.player_displayname(cn)
+            if not player_ranking then
+                player_ranking = "Unknown"
+            end
 
-    if server.player_vars(cn).reserved_name then
-        name = green(server.player_name(cn)) .. " (" .. cn .. "): " .. yellow(server.player_vars(cn).reserved_name)
-    elseif server.player_vars(cn).stats_auth_name then
-        name = green(server.player_name(cn)) .. " (" .. cn .. "): " .. yellow(server.player_vars(cn).stats_auth_name)
-    end
+            local name = server.player_displayname(tcn)
 
-    if server.player_priv_code(cn) > server.PRIV_NONE then
-        name = name .. "(" .. server.player_priv(cn) .. ")"
-    end
+            if server.player_vars(tcn).reserved_name then
+                name = green(server.player_name(tcn)) .. " (" .. tcn .. "): " .. yellow(server.player_vars(tcn).reserved_name)
+            elseif server.player_vars(tcn).stats_auth_name then
+                name = green(server.player_name(tcn)) .. " (" .. tcn .. "): " .. yellow(server.player_vars(tcn).stats_auth_name)
+            end
 
-    local normal_message = string.format(server.client_connect_message, name, city, country, player_ranking)
-    local admin_message = string.format(server.client_connect_admin_message, normal_message, server.player_ip(cn))
+            if server.player_priv_code(tcn) > server.PRIV_NONE then
+                name = name .. "(" .. server.player_priv(tcn) .. ")"
+            end
 
-    for _, cn in ipairs(server.clients()) do
+            local normal_message = string.format(server.client_connect_message, name, city, country, player_ranking)
+            local admin_message = string.format(server.client_connect_admin_message, normal_message, server.player_ip(tcn))
 
-        local message = normal_message
+            for _, cn in ipairs(server.clients()) do
 
-        if server.player_priv_code(cn) == server.PRIV_ADMIN then
-            message = admin_message
-        end
+                local message = normal_message
 
-        server.player_msg(cn, message)
-    end
+                if server.player_priv_code(cn) == server.PRIV_ADMIN then
+                    message = admin_message
+                end
+
+                server.player_msg(cn, message)
+            end
+            server.player_vars(tcn).shown_connect = true
+
 end
 
 local function sendServerBanner(cn)
@@ -55,21 +59,30 @@ local function sendServerBanner(cn)
     server.player_vars(cn).maploaded = true
     
     if server.player_vars(cn).shown_banner then return end
-    
-    local sid = server.player_sessionid(cn)
-    
-    server.sleep(BANNER_DELAY, function()
+    if true == server.player_vars(cn).shown_connect then return end
 
-        -- cancel if not the same player from 1 second ago
-        if sid ~= server.player_sessionid(cn) then return end
-        
-        server.player_msg(cn, server.motd)
-        server.player_msg(cn, server.connect_info_message)
+    server.interval(1000, function()
 
-        server.player_vars(cn).shown_banner = true
+        if not server.gamepause then
+            local sid = server.player_sessionid(cn)
 
-        send_connect_message(cn)
+                server.sleep(BANNER_DELAY, function()
+
+                -- cancel if not the same player from 1 second ago
+                if sid ~= server.player_sessionid(cn) then return -1 end
+                if true == server.player_vars(cn).shown_connect then return -1 end
+
+                server.player_msg(cn, server.motd)
+                server.player_msg(cn, server.connect_info_message)
+
+                server.player_vars(cn).shown_banner = true
+
+                send_connect_message(cn)
+            end)
+            return -1
+        end
     end)
+
 end
 
 server.event_handler("maploaded", sendServerBanner)
