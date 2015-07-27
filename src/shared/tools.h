d61023e1edacdc629a18120b76244d69f816b3d8
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2015-07-27 23:03:40 +0000

    hashset add fix hashnameset cleanups sound cleanups GL_RGBA16F_ARB -> GL_RGBA16F require framebuffer objects require texture rectangles remove depthfxrect and depthfxfilter vars remove fixed-function matrix stack and fog state
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@5168 07af1644-1164-4067-b897-63bf4dc9b748
    (cherry picked from commit ff43074f93da01b2b485358d3a05f18871357d32)

--- src/shared/tools.h
+++ src/shared/tools.h
@@ -862,13 +862,6 @@ template<class H, class E, class K, class T> struct hashbase
         HTFIND( , insert(h, key) = elem);
     }
 
-    template<class V>
-    T &add(const V &elem)
-    {
-        const K &key = H::getkey(elem);
-        HTFIND( , insert(h, key) = elem);
-    }
-
     template<class U>
     T &operator[](const U &key)
     {
@@ -886,7 +879,6 @@ template<class H, class E, class K, class T> struct hashbase
     {
         HTFIND( , notfound);
     }
-
 
     template<class U>
     bool remove(const U &key)
@@ -940,6 +932,12 @@ template<class T> struct hashset : hashbase<hashset<T>, T, T, T>
     static inline const T &getkey(const T &elem) { return elem; }
     static inline T &getdata(T &elem) { return elem; }
     template<class K> static inline void setkey(T &elem, const K &key) {}
+
+    template<class V>
+    T &add(const V &elem)
+    {
+        return basetype::access(elem, elem);
+    }
 };
 
 template<class T> struct hashnameset : hashbase<hashnameset<T>, T, const char *, T>
@@ -952,6 +950,12 @@ template<class T> struct hashnameset : hashbase<hashnameset<T>, T, const char *,
     template<class U> static inline const char *getkey(U *elem) { return elem->name; }
     static inline T &getdata(T &elem) { return elem; }
     template<class K> static inline void setkey(T &elem, const K &key) {}
+
+    template<class V>
+    T &add(const V &elem)
+    {
+        return basetype::access(getkey(elem), elem);
+    }
 };
 
 template<class K, class T> struct hashtableentry
