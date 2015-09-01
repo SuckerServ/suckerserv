94528cd94505de444ddbb1bc4ff31bb99fb0744a
piernov <piernov@piernov.org>
2015-09-01 22:24:46 +0200

    Fix for multiple spies
    (cherry picked from commit 30577567bc85b9acb60af02c2972f9908a093b40)

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -601,9 +601,8 @@ namespace server
     {
         if(n >= spycn)
         {
-            n -= spycn;
             extern vector<clientinfo *> spies;
-            if(spies.inrange(n)) return spies[n];
+            loopv(spies) if(spies[i]->clientnum == n) return spies[i];
         }
         if(n < MAXCLIENTS) return (clientinfo *)getclientinfo(n);
         n -= MAXCLIENTS;
@@ -680,7 +679,7 @@ namespace server
             if(ci->messages.length() > 0) ci->messages.shrink(0);
             ci->spy = false;
             ci->hide_privilege = false;
-            spies.remove(spycn - ci->clientnum);
+            spies.removeobj(ci);
             ci->clientnum = ci->n;
             sendservinfo(ci);
             sendinitclient(ci);
