942b14febaed902b0c81c7149df06e59e5fc1826
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2020-04-12 16:31:08 +0000

    warning fixes
    git-svn-id: svn://svn.code.sf.net/p/sauerbraten/code@5873 07af1644-1164-4067-b897-63bf4dc9b748
    (cherry picked from commit a42e1f7cdf1b75ed25e6b3e104cdf4575d995e74)

--- src/shared/crypto.cpp
+++ src/shared/crypto.cpp
@@ -291,7 +291,7 @@ template<int BI_DIGITS> struct bigint
 
     template<int Y_DIGITS> void copyshrinkdigits(const bigint<Y_DIGITS> &y, int n)
     {
-        len = min(y.len, n);
+        len = clamp(y.len, 0, n);
         memcpy(digits, y.digits, len*sizeof(digit));
         shrink();
     }
@@ -369,7 +369,7 @@ template<int BI_DIGITS> struct bigint
     
     template<int Y_DIGITS> void copydigits(int to, const bigint<Y_DIGITS> &y, int from, int n)
     {
-        int avail = min(y.len-from, n);
+        int avail = clamp(y.len-from, 0, n);
         memcpy(&digits[to], &y.digits[from], avail*sizeof(digit));
         if(avail < n) memset(&digits[to+avail], 0, (n-avail)*sizeof(digit));
     }
