fc34cc709969440e817ff1cdde17694c9327003e
piernov <piernov@piernov.org>
2020-12-05 22:13:27 +0100

    add map nessus

--- conf/maps.lua
+++ conf/maps.lua
@@ -11,7 +11,7 @@ server.ffa_maps = {
 "gubo", "oldschool", "teahupoo", "force", "rm1", "asenatra", "pariah", "access", "albatross", "exist", "stronhold",
 "toxicity", "frag-lab", "aod", "sauerowalk", "anubis", "lost_soul", "gorge", "headroom", "hdm3", "waltz", "colony",
 "insipid", "zavial", "phrantic", "hektik", "ferguson", "exo", "abyss", "fallen", "memoria", "ventania", "zdm2", "turmoil",
-"conflict", "purgatory", "souls", "clash", "hator", "haze", "ghetto"
+"conflict", "purgatory", "souls", "clash", "hator", "haze", "ghetto", "nessus"
 }
 
 server.small_ffa_maps = {
@@ -63,7 +63,7 @@ server.capture_maps = {
 "subterra", "infamy", "capture_night", "river_c", "serenity", "xenon", "nmp9", "ph-capture",
 "killcore3", "twinforts", "duomo", "donya", "asgard", "eternal_valley", "mill", "nucleus", "laucin", "stronhold", "luna", "aod",
 "mc-lab", "fragnostic", "eris", "fallen", "genesis", "new_energy", "disruption", "bklyn", "river_keep", "waltz", "gorge", "anubis",
-"ruby", "ow"
+"ruby", "ow", "nessus"
 }
 
 server.small_capture_maps = {
