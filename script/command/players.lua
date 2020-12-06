0bafc1dbacea1bba2745187186a2e29678a5641a
piernov <piernov@piernov.org>
2020-12-06 23:10:49 +0100

    Fix mmdb call in players command

--- script/command/players.lua
+++ script/command/players.lua
@@ -4,8 +4,8 @@
 
 return function(cn)
     for p in server.gplayers() do
-        local country = mmdb.lookup_ip(p:ip(), "country", "names", "en")
-        local city = mmdb.lookup_ip(p:ip(), "city", "names", "en")
+        local country = server.mmdatabase:lookup_ip(p:ip(), "country", "names", "en")
+        local city = server.mmdatabase:lookup_ip(p:ip(), "city", "names", "en")
         if not country or #country < 1 then country = "Unknown" end
         if not city or #city < 1 then city = "Unknown" end
         server.player_msg(cn, "player_list", { name = p:displayname(), city = city, country = country, frags = p:frags(), deaths = p:deaths(), acc = p:accuracy() })
